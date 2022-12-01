be63c0c2c9bfe6eb95414c7c2748412e92d0376b
Gabriela Bohorquez <gaby030300@hotmail.com>
2022-12-01 16:30:11 -0500

    DoTween - Card Movement
    DoTween - Card Movement

--- Assets/Scripts/CardMovement.cs
+++ Assets/Scripts/CardMovement.cs
@@ -6,28 +6,38 @@ using DG.Tweening;
 
 public class CardMovement : MonoBehaviour
 {
-    SoundManager soundManager;
+    public bool isUp;
+    public int id;
     void Start()
-    {
-        soundManager = GetComponent<SoundManager>();
+    {        
         DOTween.Init(true, true, LogBehaviour.Verbose).SetCapacity(200, 10);
     }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+    private void OnMouseDown()
+    {        
+        if (isUp)
+        {
+            CardDown();
+        }
+        else
         {
             CardUp();
         }
     }
+
+    [ContextMenu("CardUp")]
     public void CardUp()
-    {
-        soundManager.audioSource.PlayOneShot(soundManager.swipeEffect, 0.5f);
-        transform.DOMove(new Vector3(transform.position.x, 2, transform.position.z), 1);
-        transform.DORotate(new Vector3(0, -180, -180), 1);
+    {        
+        transform.DOMove(new Vector3(transform.position.x, 30.44f, transform.position.z), 1);
+        transform.DOLocalRotate(new Vector3(-180, 90, 0), 1);
+        isUp = true;
     }
+
+    [ContextMenu("CardDown")]
     public void CardDown()
     {
-        transform.DOMove(new Vector3(transform.position.x, 0, transform.position.z), 1);
-        transform.DORotate(new Vector3(0, -180, 0), 1);
+        transform.DOMove(new Vector3(transform.position.x, 28.44f, transform.position.z), 1);
+        transform.DOLocalRotate(new Vector3(-180, 90, 180), 1);
+        isUp = false;
     }
 }
